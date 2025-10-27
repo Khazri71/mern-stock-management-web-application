8fca538f2e2c004424f7e42fdf15016ad2173ff3
khazri71 <khazrinoura71@gmail.com>
2025-10-27 13:39:40 +0100

    deploy commit

--- gestion-stocks/vite.config.js
+++ gestion-stocks/vite.config.js
@@ -2,9 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+// npm install vite-plugin-static-copy --save-dev
+
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss(),],
-  server : {port : 3000}
+  plugins: [react() , tailwindcss(),
+
+
+     viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', // chemin du fichier source
+          dest: ''                  // destination : racine de dist
+        }
+      ]
+    })
+
+
+
+  ],
+  server : {port : 3000},
+
+
+  
 })
